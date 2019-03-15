7a75995c4e3c573d5931240c3d2b16ab80971559
HarikaRavipudi <harikaravipudi567@gmail.com>
2019-03-15 14:05:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/acer/services/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.acer.services;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void start(View view){
+        Intent i=new Intent(this,MusicService.class);
+        startService(i);
+
+    }
+    public void stop(View view){
+        Intent i=new Intent(this,MusicService.class);
+        stopService(i);
+
+    }
+}
